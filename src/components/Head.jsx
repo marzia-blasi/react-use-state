7ec728a24f3c0c4cffeaa843c89430b1466ae1f4
marzia <you@example.com>
2025-07-17 16:41:40 +0200

    add Header ad main componets

---
+++ src/components/Head.jsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <header>
+      <h1>Learn web development</h1>
+    </header>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,49 @@
+//array da cui prendere i dati
+
+const programmingLanguages = [
+  {
+    id: 1,
+    title: "JavaScript",
+    text: "Linguaggio dinamico per sviluppo web, lato client e server.",
+  },
+  {
+    id: 2,
+    title: "Python",
+    text: "Versatile, leggibile e usato per data science, web e automazione.",
+  },
+  {
+    id: 3,
+    title: "Java",
+    text: "Orientato agli oggetti, molto usato per applicazioni aziendali e Android.",
+  },
+  {
+    id: 4,
+    title: "C++",
+    text: "Potente e veloce, usato in giochi, embedded e software di sistema.",
+  },
+  {
+    id: 5,
+    title: "Go",
+    text: "Creato da Google, eccelle in performance e semplicità per backend.",
+  },
+  {
+    id: 6,
+    title: "Ruby",
+    text: "Semplice e intuitivo, noto per lo sviluppo web con Ruby on Rails.",
+  },
+];
+
+export default function Main() {
+  return (
+    <>
+      <div>prova</div>
+      <div>
+        <button>prova</button>
+        <button>prova</button>
+        <button>ptova</button>
+        <button></button>
+        <button></button>
+      </div>
+    </>
+  );
+}
