aa666afd0c19c62a72a5d73c0a5e2a6af3e46d5c
marzia <you@example.com>
2025-07-17 17:42:20 +0200

    working on main logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,49 +1,58 @@
-//array da cui prendere i dati
-
-const programmingLanguages = [
-  {
-    id: 1,
-    title: "JavaScript",
-    text: "Linguaggio dinamico per sviluppo web, lato client e server.",
-  },
-  {
-    id: 2,
-    title: "Python",
-    text: "Versatile, leggibile e usato per data science, web e automazione.",
-  },
-  {
-    id: 3,
-    title: "Java",
-    text: "Orientato agli oggetti, molto usato per applicazioni aziendali e Android.",
-  },
-  {
-    id: 4,
-    title: "C++",
-    text: "Potente e veloce, usato in giochi, embedded e software di sistema.",
-  },
-  {
-    id: 5,
-    title: "Go",
-    text: "Creato da Google, eccelle in performance e semplicità per backend.",
-  },
-  {
-    id: 6,
-    title: "Ruby",
-    text: "Semplice e intuitivo, noto per lo sviluppo web con Ruby on Rails.",
-  },
-];
+import { useState } from "react";
 
 export default function Main() {
-  return (
-    <>
-      <div>prova</div>
-      <div>
-        <button>prova</button>
-        <button>prova</button>
-        <button>ptova</button>
-        <button></button>
-        <button></button>
-      </div>
-    </>
-  );
+  const [open, isOpen] = useState(null);
+
+  function handleClick() {}
+
+  //array da cui prendere i dati
+
+  const languages = [
+    {
+      id: 1,
+      title: "JavaScript",
+      text: "Linguaggio dinamico per sviluppo web, lato client e server.",
+    },
+    {
+      id: 2,
+      title: "Python",
+      text: "Versatile, leggibile e usato per data science, web e automazione.",
+    },
+    {
+      id: 3,
+      title: "Java",
+      text: "Orientato agli oggetti, molto usato per applicazioni aziendali e Android.",
+    },
+    {
+      id: 4,
+      title: "C++",
+      text: "Potente e veloce, usato in giochi, embedded e software di sistema.",
+    },
+    {
+      id: 5,
+      title: "Go",
+      text: "Creato da Google, eccelle in performance e semplicità per backend.",
+    },
+    {
+      id: 6,
+      title: "Ruby",
+      text: "Semplice e intuitivo, noto per lo sviluppo web con Ruby on Rails.",
+    },
+  ];
+
+    return (
+            <p>prova</p>
+                {languages.map((item) => {
+                    
+                    return (
+                        <>
+                        <button>{item.title} </button>
+                            <div><p>{item.text}</p></div>
+                        </>
+                
+                )})}
+                    
+    
+    
+  )
 }
